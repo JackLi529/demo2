692146c093feb119f9ffeecac4e32f9c01216447
Jack <lyx0822@163.com>
2018-07-20 15:42:24 +0800

    Add GitHello File

--- src/main/java/com/demo/GitHello.java
+++ src/main/java/com/demo/GitHello.java
@@ -5,6 +5,9 @@ package com.demo;
  */
 public class GitHello {
     public static  void main(String args[]){
-        System.out.println("Hello Git");
+        System.out.println("Hello Git.");
+        System.out.println("Hello Git.");
+        System.out.println("Hello Git.");
+        System.out.println("Hello Git.");
     }
 }
