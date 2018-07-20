b9f0296361ce5cd70df66feae86b2982c937c79a
Jack <lyx0822@163.com>
2018-07-20 14:22:34 +0800

    Add GitHello File

---
+++ src/main/java/com/demo/GitHello.java
@@ -0,0 +1,10 @@
+package com.demo;
+
+/**
+ * Created by Jack ON 2018/7/20 14:21
+ */
+public class GitHello {
+    public static  void main(String args[]){
+        System.out.println("Hello Git");
+    }
+}
